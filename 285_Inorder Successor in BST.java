e3c2af7a9494fcef6e6dae68a2dd440a206efbc9
Samsong <ssydyc@gmail.com>
2016-09-25 16:52:14 -0700

    Update 285_Inorder Successor in BST.java

--- 285_Inorder Successor in BST.java
+++ 285_Inorder Successor in BST.java
@@ -7,36 +7,19 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class ResultType {
-    TreeNode resNode;
-    boolean pFound; 
-    
-    public ResultType() {
-        resNode = null;
-        pFound = false;
-    }
-} 
-
 public class Solution {
-    private void successorHelper(TreeNode root, TreeNode p, ResultType res) {
-        if (root == null) {
-            return;
-        }
-        
-        successorHelper(root.left, p, res);
-        
-        if (res.pFound == false) {
-            res.pFound = (root == p);
-        } else if (res.resNode == null) {
-            res.resNode = root;
-        }
-        
-        successorHelper(root.right, p, res);
-    }
-    
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        ResultType res = new ResultType();
-        successorHelper(root, p, res);
-        return res.resNode;
+        TreeNode res = null;
+        
+        while (root != null) {
+            if (root.val <= p.val) {
+                root = root.right;
+            } else {
+                res = root;
+                root = root.left;
+            }
+        }
+        
+        return res;
     }
 }
